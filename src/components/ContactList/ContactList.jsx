cce5485d1eb45a3499fbbde65f16a6f15fc8ead5
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2023-02-15 09:39:41 +0100

    fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 // import css from './ContactList.module.css';
 
-export class ContactList extends Component {
-render() {
-      return (
+// export class ContactList extends Component {
+// render() {
+//       return (
         
-      )
-    }
-  };
+//       )
+//     }
+//   };
 
 // //   Section.propTypes = {
 // //     title: PropTypes.string.isRequired,
